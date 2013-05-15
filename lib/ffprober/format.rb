8fc001ec07b497c080bf4209a718e4986110d96a
beanieboi <beanie@benle.de>
2013-05-15 09:28:55 +0200

    don't use the setter methods from attr_accessor anymore to initialize the object, set instance vars and use attr_reader

--- lib/ffprober/format.rb
+++ lib/ffprober/format.rb
@@ -1,14 +1,11 @@
 module Ffprober
   class Format
-    attr_accessor :filename, :nb_streams, :format_name,
-                  :format_long_name, :start_time, :duration,
-                  :size, :bit_rate
+    attr_reader :filename, :nb_streams, :format_name,
+                :format_long_name, :start_time, :duration,
+                :size, :bit_rate
 
     def initialize(object_attribute_hash)
-      object_attribute_hash.map do |(k, v)|
-        writer_m = "#{k}="
-        send(writer_m, v) if respond_to?(writer_m)
-      end
+      object_attribute_hash.each {|k,v| instance_variable_set("@#{k}",v)}
     end
   end
 end
--- lib/ffprober/stream.rb
+++ lib/ffprober/stream.rb
@@ -1,16 +1,13 @@
 module Ffprober
   class Stream
-    attr_accessor :codec_name, :codec_long_name, :codec_type,
-                  :codec_time_base, :codec_tag_string, :codec_tag,
-                  :r_frame_rate, :avg_frame_rate,
-                  :time_base, :start_time, :duration,
-                  :nb_frames
+    attr_reader :codec_name, :codec_long_name, :codec_type,
+                :codec_time_base, :codec_tag_string, :codec_tag,
+                :r_frame_rate, :avg_frame_rate,
+                :time_base, :start_time, :duration,
+                :nb_frames
 
     def initialize(object_attribute_hash)
-      object_attribute_hash.map do |(k, v)|
-        writer_m = "#{k}="
-        send(writer_m, v) if respond_to?(writer_m)
-      end
+      object_attribute_hash.each {|k,v| instance_variable_set("@#{k}",v)}
     end
   end
 end
