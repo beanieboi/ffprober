b9111195955f3ffb6d39c3ab03424c88b62bf837
beanieboi <beanie@benle.de>
2013-06-01 16:31:36 +0200

    try to detect windows and add '.exe' to the executable name

--- lib/ffprober.rb
+++ lib/ffprober.rb
@@ -10,12 +10,17 @@ require "json"
 module Ffprober
   def self.path
     name = 'ffprobe'
+    name << '.exe' if self.windows?
 
     path = ENV['PATH'].split(File::PATH_SEPARATOR).find do |path|
       File.executable?(File.join(path, name))
     end
 
     path && File.expand_path(name, path)
+  end
+
+  def self.windows?
+    RUBY_PLATFORM =~ /(mingw|mswin)/
   end
 
   class InvalidInputFileError < ::StandardError; end
