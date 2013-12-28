2750a6cc20c7e33716d89d820dd4a536cc9feb9b
beanieboi <beanie@benle.de>
2013-12-28 13:00:39 +0100

    refine parser, better naming, parse json directly

--- lib/ffprober/parser.rb
+++ lib/ffprober/parser.rb
@@ -2,55 +2,47 @@ module Ffprober
   class Parser
     @@options = '-v quiet -print_format json -show_format -show_streams'
 
-    class << self
-      def from_file(file_to_parse)
-        unless FfprobeVersion.valid?
-          raise ArgumentError.new("no or unsupported ffprobe version found.\
-                                  (version: #{FfprobeVersion.new.version.to_s})")
-        end
-
-        json_output = `#{Ffprober.path} #{@@options} '#{file_to_parse}'`
-        from_json(json_output)
+    def self.from_file(file_to_parse)
+      unless FfprobeVersion.valid?
+        raise ArgumentError.new("no or unsupported ffprobe version found.\
+                                (version: #{FfprobeVersion.new.version.to_s})")
       end
 
-      def from_json(json_to_parse)
-        parser = self.new
-        parser.parse(json_to_parse)
-        parser
-      end
+      json_output = `#{Ffprober.path} #{@@options} '#{file_to_parse}'`
+      from_json(json_output)
+    end
+
+    def self.from_json(json_to_parse)
+      parser = self.new
+      parser.parse(json_to_parse)
+      parser
     end
 
     def parse(json_to_parse)
-      raise ArgumentError.new("No JSON found") if json_to_parse.nil?
-      @json_to_parse = json_to_parse
-    end
-
-    def parsed_json
-      @parsed_json ||=  begin
-                          json = JSON.parse(@json_to_parse, symbolize_names: true)
-                          raise InvalidInputFileError.new("Invalid input file") if json.empty?
-                          json
-                        end
+      raise ArgumentError.new("No JSON input data") if json_to_parse.nil?
+      @json = JSON.parse(json_to_parse, symbolize_names: true)
     end
 
     def format
-      @format ||= Ffprober::Format.new(parsed_json[:format])
+      @format ||= Ffprober::Format.new(@json[:format])
     end
 
     def video_streams
-      @video_streams ||= stream_by_codec('video').map { |s| Ffprober::VideoStream.new(s) }
+      @video_streams ||= stream_by_codec('video').map { |stream| Ffprober::VideoStream.new(stream) }
     end
 
     def audio_streams
-      @audio_streams ||= stream_by_codec('audio').map { |s| Ffprober::AudioStream.new(s) }
+      @audio_streams ||= stream_by_codec('audio').map { |stream| Ffprober::AudioStream.new(stream) }
     end
+
+    private
 
     def stream_by_codec(codec_type)
       streams.select { |stream| stream[:codec_type] == codec_type }
     end
 
     def streams
-      parsed_json[:streams]
+      @json[:streams]
     end
   end
 end
