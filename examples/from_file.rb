00c90aab7ae76eb45d8d5cb34b5b59372a8637a3
beanieboi <beanie@benle.de>
2015-04-25 14:33:20 +0200

    some hound fixes

--- examples/from_file.rb
+++ examples/from_file.rb
@@ -1,4 +1,4 @@
 require_relative '../lib/ffprober'
 
-parser = Ffprober::Parser.from_file('../spec/assets/sample video.m4v')
+parser = Ffprober::Parser.from_file("../spec/assets/sample video.m4v")
 puts parser.video_streams.inspect
--- lib/ffprober.rb
+++ lib/ffprober.rb
@@ -1,22 +1,22 @@
-require_relative 'ffprober/version'
-require_relative 'ffprober/dynamic_initializer'
-require_relative 'ffprober/parser'
-require_relative 'ffprober/format'
-require_relative 'ffprober/stream'
-require_relative 'ffprober/audio_stream'
-require_relative 'ffprober/video_stream'
-require_relative 'ffprober/subtitle_stream'
-require_relative 'ffprober/chapter'
-require_relative 'ffprober/ffprobe_version'
-require_relative 'ffprober/wrapper'
-require_relative 'ffprober/errors'
+require_relative "ffprober/version"
+require_relative "ffprober/dynamic_initializer"
+require_relative "ffprober/parser"
+require_relative "ffprober/format"
+require_relative "ffprober/stream"
+require_relative "ffprober/audio_stream"
+require_relative "ffprober/video_stream"
+require_relative "ffprober/subtitle_stream"
+require_relative "ffprober/chapter"
+require_relative "ffprober/ffprobe_version"
+require_relative "ffprober/wrapper"
+require_relative "ffprober/errors"
 
-require_relative 'ffprober/ffmpeg/finder'
-require_relative 'ffprober/ffmpeg/version'
-require_relative 'ffprober/ffmpeg/version_validator'
-require_relative 'ffprober/parsers/file'
-require_relative 'ffprober/parsers/json'
-require 'json'
+require_relative "ffprober/ffmpeg/finder"
+require_relative "ffprober/ffmpeg/version"
+require_relative "ffprober/ffmpeg/version_validator"
+require_relative "ffprober/parsers/file"
+require_relative "ffprober/parsers/json"
+require "json"
 
 module Ffprober
 end
