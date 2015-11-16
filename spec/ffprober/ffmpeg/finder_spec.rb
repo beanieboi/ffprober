aa59d6ad3539b98dbbaa4e22332d5ce798165ef7
beanieboi <beanie@benle.de>
2015-11-16 11:49:38 -0600

    remove path

--- spec/ffprober/ffmpeg/finder_spec.rb
+++ spec/ffprober/ffmpeg/finder_spec.rb
@@ -2,7 +2,6 @@ require "spec_helper"
 
 RSpec.describe Ffprober::Ffmpeg::Finder do
   it "should raise a exception if there is no ffmpeg" do
-    ENV['PATH'] = ""
     expect do
       Ffprober::Parser.from_file("spec/assets/301-extracting_a_ruby_gem.m4v")
     end.to raise_error(Ffprober::UnsupportedVersion)
