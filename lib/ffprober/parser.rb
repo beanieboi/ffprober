ae816d3a2bddaa87f4cc95dc912c7871599f41e3
ben <beanie@benle.de>
2020-11-28 19:32:38 +0100

    Update parser

--- lib/ffprober/parser.rb
+++ lib/ffprober/parser.rb
@@ -1,8 +1,11 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 module Ffprober
   class Parser
+    extend T::Sig
+
+    sig {params(file_to_parse: String).returns(Ffprober::Wrapper)}
     def self.from_file(file_to_parse)
       check_version
 
@@ -13,6 +16,7 @@ module Ffprober
       Ffprober::Wrapper.new(json_parser.json)
     end
 
+    sig {params(url_to_parse: T.untyped).returns(Ffprober::Wrapper)}
     def self.from_url(url_to_parse)
       check_version
 
@@ -21,11 +25,13 @@ module Ffprober
       Ffprober::Wrapper.new(json_parser.json)
     end
 
+    sig {params(json_to_parse: T.untyped).returns(Ffprober::Wrapper)}
     def self.from_json(json_to_parse)
       json_parser = Parsers::JsonParser.new(json_to_parse)
       Ffprober::Wrapper.new(json_parser.json)
     end
 
+    sig {returns(NilClass)}
     def self.check_version
       msg = "found version: #{FfprobeVersion.version}"
       raise UnsupportedVersion, msg if FfprobeVersion.invalid?
