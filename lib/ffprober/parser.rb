c0d25285dc478fbc390bf79f716da9b1f32dfa67
Ben Fritsch <beanie@benle.de>
2025-02-25 06:15:24 +0100

    remove Sorbet (#111)

--- lib/ffprober/parser.rb
+++ lib/ffprober/parser.rb
@@ -3,9 +3,6 @@
 
 module Ffprober
   class Parser
-    extend T::Sig
-
-    sig { params(file_to_parse: String).returns(Ffprober::Wrapper) }
     def self.from_file(file_to_parse)
       check_version
 
@@ -16,7 +13,6 @@ module Ffprober
       Ffprober::Wrapper.new(json_parser.json)
     end
 
-    sig { params(url_to_parse: T.untyped).returns(Ffprober::Wrapper) }
     def self.from_url(url_to_parse)
       check_version
 
@@ -25,13 +21,11 @@ module Ffprober
       Ffprober::Wrapper.new(json_parser.json)
     end
 
-    sig { params(json_to_parse: T.untyped).returns(Ffprober::Wrapper) }
     def self.from_json(json_to_parse)
       json_parser = Parsers::JsonParser.new(json_to_parse)
       Ffprober::Wrapper.new(json_parser.json)
     end
 
-    sig { returns(NilClass) }
     def self.check_version
       msg = "found version: #{FfprobeVersion.version}"
       raise UnsupportedVersion, msg if FfprobeVersion.invalid?
